2e521e6d30d0c612ee7fd798e6d381fcc9b81bfa
Барышев Эмиль Евгеньевич <211904@mck-ktits.ru>
2021-04-21 12:50:59 +0300

    Добавьте файлы проекта.

---
+++ Circle/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Circle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            if(form2.ShowDialog(this) == DialogResult.OK)
+            {
+                var paper = panel1.CreateGraphics();
+                var pen = new Pen(Color.Pink, 7);
+                if (form2.TextBox1.Text != null && form2.TextBox2.Text != null && form2.TextBox3.Text != null)
+                {
+                    paper.DrawEllipse(pen, Convert.ToInt32(form2.TextBox1.Text), Convert.ToInt32(form2.TextBox2.Text), Convert.ToInt32(form2.TextBox3.Text), Convert.ToInt32(form2.TextBox3.Text));
+                }
+                
+            }
+        }
+    }
+}
---
+++ Circle/Form2.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Circle
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.TextBox3 = new System.Windows.Forms.TextBox();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(92, 121);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(15, 13);
+            this.Label3.TabIndex = 13;
+            this.Label3.Text = "R";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(92, 64);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(14, 13);
+            this.Label2.TabIndex = 12;
+            this.Label2.Text = "Y";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(92, 16);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(14, 13);
+            this.Label1.TabIndex = 11;
+            this.Label1.Text = "X";
+            // 
+            // Button1
+            // 
+            this.Button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.Button1.Location = new System.Drawing.Point(196, 80);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(75, 23);
+            this.Button1.TabIndex = 10;
+            this.Button1.Text = "Ok";
+            this.Button1.UseVisualStyleBackColor = true;
+            // 
+            // TextBox3
+            // 
+            this.TextBox3.Location = new System.Drawing.Point(44, 137);
+            this.TextBox3.Name = "TextBox3";
+            this.TextBox3.Size = new System.Drawing.Size(100, 20);
+            this.TextBox3.TabIndex = 9;
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Location = new System.Drawing.Point(44, 80);
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(100, 20);
+            this.TextBox2.TabIndex = 8;
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(44, 32);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(100, 20);
+            this.TextBox1.TabIndex = 7;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 190);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.TextBox3);
+            this.Controls.Add(this.TextBox2);
+            this.Controls.Add(this.TextBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Button Button1;
+        internal System.Windows.Forms.TextBox TextBox3;
+        internal System.Windows.Forms.TextBox TextBox2;
+        internal System.Windows.Forms.TextBox TextBox1;
+    }
+}
